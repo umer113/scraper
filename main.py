5f25a6654af2f54d1fee846c0176795eada12cbc
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2024-10-23 13:45:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ class BinaAzScraper:
 
 if __name__ == "__main__":
     start_url = 'https://bina.az/kiraye/menziller/kohne-tikili'
-    start_page = 401
-    end_page = 566
+    start_page = 1
+    end_page = 400
     scraper = BinaAzScraper(start_url, start_page, end_page)
     scraper.run()
