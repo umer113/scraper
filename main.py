767b2c8f90c11031de6bbf8af698c73ca883868c
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2024-10-27 16:49:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class BinaAzScraper:
         area = next((span.get_text() for span in soup.select('span.product-properties__i-value') if 'm²' in span.get_text()), None)
         
         # Static property type and transaction type
-        property_type = "house"
+        property_type = "old building apartment"
         transaction_type = "rent"
 
         if transaction_type == 'sale':
@@ -113,8 +113,8 @@ class BinaAzScraper:
             time.sleep(2)  # Add a delay of 2 seconds between each request
 
 if __name__ == "__main__":
-    start_url = 'https://bina.az/kiraye/heyet-evleri'
+    start_url = 'https://bina.az/kiraye/menziller/kohne-tikili'
     start_page = 1
-    end_page = 102
+    end_page = 115
     scraper = BinaAzScraper(start_url, start_page, end_page)
     scraper.run()
