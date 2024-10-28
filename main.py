ad5b3105420df4f756bbf6fade9daf0f7718e666
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2024-10-29 02:23:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def scrape_listings(urls):
 
 # List of URLs of the webpages to scrape
 urls = [
-    "https://www.propertyfinder.bh/en/buy/properties-for-sale.html"
+    "https://www.propertyfinder.bh/en/search?c=1&bdr[]=1&btr[]=1&fu=0&am[]=AC&ob=mr"
     # Add your URLs here
 ]
 
