e9c1cddcbd1ea3b0db7a96ea8b4135ed523eed2f
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2025-01-31 22:31:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ class BinaAzScraper:
 
 if __name__ == "__main__":
     start_url = 'https://bina.az/alqi-satqi/menziller/yeni-tikili'
-    start_page = 351
-    end_page = 700
+    start_page = 701
+    end_page = 1000
     scraper = BinaAzScraper(start_url, start_page, end_page)
     scraper.run()
