151988277f571ce11936b80656e2372fb226798a
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2025-02-20 19:07:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class BinaAzScraper:
         self.save_all_data_to_excel()
 
 if __name__ == "__main__":
-    start_url = 'https://bina.az/alqi-satqi/heyet-evleri'
+    start_url = 'https://bina.az/alqi-satqi/ofisler'
     start_page = 1
     end_page = 5
     scraper = BinaAzScraper(start_url, start_page, end_page)
